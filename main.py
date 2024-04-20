4c30024821ce05d3e1391d8c036e2dd89cbbb276
KabilanMA <kabilanen@gmail.com>
2024-04-20 15:46:58 +0530

    tool completed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.Bugs import Bug
 from src.KubernetesBugs import KubernetesBug
+from src.main import kubectl_delete
+import os, json
 
 if __name__ == "__main__":
-    bug1 = KubernetesBug("./bugs/bug-1", False)
-    bug1.bugDefine()
+    patch = os.path.join(".", "bugs", "bug-1", "patch.json")
+    # kubectl_delete(patch)
+    with open(patch, "r") as file:
+        patchData = json.load(file)
+    print(patchData['metadata']['name'])
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import time
 import os
+import tkinter.messagebox
+import subprocess
+import json
 
 
 def find_subdirectory_by_number(parent_directory, number):
@@ -8,6 +11,81 @@ def find_subdirectory_by_number(parent_directory, number):
         if subdir.startswith("bug-") and subdir.split("-")[1] == str(number):
             return os.path.join(parent_directory, subdir)
     return None
+
+def kubectl_apply(manifest_path: str) -> bool:
+    command = ["kubectl", "apply", "-f", manifest_path]
+    try:
+        result = subprocess.run(command, check=True)
+        print("Resource deployed successfully.")
+        print("Subprocess output:")
+        print(result.stdout)
+        return True
+    except subprocess.CalledProcessError as err:
+        print(f"Error deploying resource: {err}")
+        return False
+
+def kubectl_delete(patchJson:dict, namespace=None)->bool:
+    resource_type = patchJson['kind']
+    resource_name = patchJson['metadata']['name']
+
+    command = ["kubectl", "delete", resource_type, resource_name]
+    if namespace:
+        command.extend(["--namespace", namespace])
+    try:
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        print("Resource deleted successfully.")
+        print("Subprocess output:")
+        print(result.stdout)
+        return True
+    except subprocess.CalledProcessError as err:
+        print(f"Error deleting resource: {err}")
+        print("Subprocess error output:")
+        print(err.stderr)
+        return False
+
+def deploy_kubernetes_resource(manifest: str, text_widget: tk.Text, new_window: tk.Toplevel) -> bool:
+    if (kubectl_apply(manifest)):
+        text_widget.insert(tk.END, "Resource deployed Successfully\n")
+        new_window.update()
+        time.sleep(1)
+        text_widget.insert(tk.END, "Will patch the resource to reproduce the error in 10 seconds.\n")
+        new_window.update()
+        time.sleep(10)
+        return True
+    else:
+        text_widget.insert(tk.END, "Resource deployment failed\nPlease Check all the requirements.\n")
+        new_window.update()
+        time.sleep(1)
+        return False
+    
+def deploy_kubernetes_patch(patch: str, text_widget: tk.Text, new_window: tk.Toplevel) -> bool:
+    if (kubectl_apply(patch)):
+        text_widget.insert(tk.END, "Buggy Patch Created!\nYou can view you resource via kubectl\n")
+        new_window.update()
+        time.sleep(3)
+        toDelete = tkinter.messagebox.askyesno("Delete Resource", "You want to delete the resource?")
+        if (toDelete):
+            with open(patch, "r") as file:
+                patchJson = json.load(file)
+            counter = 20
+            while ((not (kubectl_delete(patchJson))) and counter>0):
+                counter-=1
+            if counter == 0:
+                text_widget.insert(tk.END, "Error Deleting the resource\nDelete the resource manually using kubectl\n")
+                new_window.update()
+                time.sleep(1)
+                return False
+            return True
+        else:
+            text_widget.insert(tk.END, "Delete the resource manually when necessary\n")
+            new_window.update()
+            time.sleep(1)
+            return True
+    else:
+        text_widget.insert(tk.END, "Patch deployment failed\nPlease Check all the requirements.\n")
+        new_window.update()
+        time.sleep(1)
+        return False
 
 
 def handleSubmit(
@@ -52,7 +130,6 @@ def handleSubmit(
             tk.END, "======================================================\n"
         )
         new_window.update()
-        new_window.update()
         time.sleep(1)
         if definitionData["resolved"] == "open":
             text_widget.insert(
@@ -85,6 +162,10 @@ def handleSubmit(
         text_widget.insert(tk.END, "\n")
         new_window.update()
         time.sleep(1)
+
+        if toReproduce:
+            toReproduce = tkinter.messagebox.askyesno("Reproduce Bug", "Do you have the required version installed to reproduce the bug?") and toReproduce
+
         text_widget.insert(
             tk.END, "======================================================\n"
         )
@@ -94,9 +175,10 @@ def handleSubmit(
         text_widget.insert(
             tk.END, "======================================================\n"
         )
-        text_widget.insert(
-            tk.END, "Use the below manifest to create Kubernetes Resource:\n"
-        )
+        if toReproduce:
+            text_widget.insert(tk.END, "Using the below manifest to create Kubernetes Resource")
+        else:
+            text_widget.insert(tk.END, "Use the below manifest to create Kubernetes Resource:\n")
         new_window.update()
         time.sleep(1)
         pretty_json = json.dumps(manifestData, indent=4)
@@ -107,7 +189,15 @@ def handleSubmit(
         text_widget.insert(
             tk.END, "======================================================\n"
         )
-        text_widget.insert(tk.END, "Modify the manifest as below\n")
+
+        is_deployed = False
+        if toReproduce:
+            is_deployed = deploy_kubernetes_resource(manifest, text_widget, new_window)
+        
+        if is_deployed:
+            text_widget.insert(tk.END, "Modifying the manifest as below\n")
+        else:
+            text_widget.insert(tk.END, "Modify the manifest as below\n")
         text_widget.insert(
             tk.END, "======================================================\n"
         )
@@ -116,16 +206,16 @@ def handleSubmit(
         pretty_json = json.dumps(patchData, indent=4)
         text_widget.insert(tk.END, pretty_json)
         text_widget.insert(tk.END, "\n")
+        new_window.update()
+        time.sleep(1)
+        
+        if is_deployed:
+            deploy_kubernetes_patch(patch, text_widget, new_window)
+
     else:
-        print(f"No subdirectory found for bug-{bugId}")
-    # for i in range(10):
-    #     text_widget.insert(tk.END, f"Statement {i}\n")
-    #     new_window.update()  # Update the window to show the new text
-    #     time.sleep(1)  # Sleep for 1 second between adding each line
-
-
-import json
-
+        text_widget.insert(tk.END, f"No subdirectory found for bug-{bugId}\n")
+        new_window.update()
+        time.sleep(2)
 
 def printDefinitionK8s(definition: dict, manifest: dict, patch: dict) -> None:
     print("====================BUG Definition====================")
